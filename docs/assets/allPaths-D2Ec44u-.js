141a22ea4b9d82452fefa9d4952792ce55eaf7ad
Per-Erik Karlsson <per-erik.karlsson@tietoevry.com>
2026-06-16 09:04:28 +0200

    build

---
+++ docs/assets/allPaths-D2Ec44u-.js
@@ -0,0 +1,1 @@
+import{I as s}from"./index-DtzT8OsX.js";import{I as r}from"./index-mbPvlb_I.js";import{p as n,I as c}from"./index-Cpwtqyuf.js";function p(t,a){const o=n(t);return a===c.STANDARD?s[o]:r[o]}export{s as IconSvgPaths16,r as IconSvgPaths20,p as getIconPaths};
---
+++ docs/assets/allPathsLoader-tOmERpCN.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./allPaths-D2Ec44u-.js","./index-DtzT8OsX.js","./index-mbPvlb_I.js","./index-Cpwtqyuf.js","./index-Nrk4uysg.css"])))=>i.map(i=>d[i]);
+import{_ as e}from"./index-Cpwtqyuf.js";const s=async(t,a)=>{const{getIconPaths:o}=await e(async()=>{const{getIconPaths:r}=await import("./allPaths-D2Ec44u-.js");return{getIconPaths:r}},__vite__mapDeps([0,1,2,3,4]),import.meta.url);return o(t,a)};export{s as allPathsLoader};
